e415118663b3eb471e4ae7211849098cf056bdfe
James Rchrdsn <laurent.james63@gmail.com>
2024-05-07 19:43:47 +0200

    form almost

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,6 +19,7 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
